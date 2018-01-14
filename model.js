b9ee70d523834901f015307670808eb4dd5ac6f2
Jan Wolff <janw@mailbox.org>
2018-01-14 19:04:04 +0100

    less hardcoded stuff, dynamic questions with knockout

--- model.js
+++ model.js
@@ -1,8 +1,41 @@
 var model = null;
 
+function addCourse(name) {
+	model.courses.push({name: name});
+}
+
+function deselectCourse() {
+	model.viewCourse(null);
+}
+
+function selectCourse(name) {
+	model.viewCourse(name);
+}
+
+function addQuestion(name, author, course, comments, score) {
+	var question = {name: name,
+					author: author,
+					course: course,
+					comments: comments,
+					score: score};
+	model.questions.push(question);
+}
+
 var ViewModel = function() {
+	var self = this; // "this" context in javascript is a fucking joke
 	this.username = ko.observable("student123");
-	this.view = ko.observable(null); // null -> alle kurse. nicht null -> name vom momentanen kurs
+	this.viewCourse = ko.observable(null); // null -> alle kurse. nicht null -> name vom momentanen kurs
+	this.courses = ko.observableArray();
+	this.questions = ko.observableArray();
+	this.courseQuestions = ko.pureComputed(function() {
+		var visibleQuestions = [];
+		self.questions().forEach(q => {
+			if(q.course === self.viewCourse()) {
+				visibleQuestions.push(q);
+			}
+		});
+		return visibleQuestions;
+	})
 };
 
 $(document).ready(function() {
