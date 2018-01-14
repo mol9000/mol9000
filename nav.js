3e4f34e3b853a9f8c0a80bdad6b54b550612f327
Jannik <jannikmeinecke@web.de>
2018-01-14 20:57:19 +0100

    add navbar

---
+++ nav.js
@@ -0,0 +1,44 @@
+
+
+
+
+function gotoModule(id){
+
+    //server yadayada mit id
+    var text="Programmieren 1"
+
+    $("#gt1").show()
+    $("#moduleli").show();
+    $("#modulelink").text(text);
+    $("#modulelink").click();
+
+}
+function gotoQuestion(id){
+
+
+    //server yadayada mit id
+    var text="Bitte hilf mir"
+    $("#gt2").show()
+    $("#questionli").show();
+    $("#questionlink").text(text);
+    $("#questionlink").click();
+
+}
+
+function navAppend(){
+
+}
+
+$(function(){
+    $("#modulelink").on("click", function(){
+        $("#gt2").hide()
+        $("#questionli").hide();
+    });
+    $("#dropdownMenuButton").on("click", function(){
+        $("#gt1").hide()
+        $("#moduleli").hide();
+        $("#gt2").hide()
+        $("#questionli").hide();
+    });
+})
+
